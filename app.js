99520aa5dd8a85b913a54c9ef15564eb618915e3
Benito Lopez <bennobenno2730@gmail.com>
2023-07-19 00:46:47 -0600

    agregando articles con metodo POST desde postman

--- app.js
+++ app.js
@@ -10,22 +10,48 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(bodyParser.json()); // Agrega esta línea para admitir JSON en el cuerpo de la solicitud
+
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://127.0.0.1:27017/wikiDB", {
   useNewUrlParser: true
 });
 
+const articleSchema = {
+  title: String,
+  content: String
+};
 
+const Article = mongoose.model("Article", articleSchema);
 
+app.get("/articles", (req, res) => {
+  Article.find()
+    .then(foundArticles => {
+      res.json(foundArticles);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
 
-const articleSchema={
-    title:String,
-    content:String
-}
+app.post("/article", async (req, res) => {
+  try {
+    const newTopic = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
 
-const article =mongoose.model("Article",articleSchema)
+    await newTopic.save();
+    console.log(newTopic);
+
+    res.send("Artículo guardado exitosamente.");
+  } catch (error) {
+    console.error(error);
+    res.send("Error al guardar el artículo.");
+  }
+});
 
 app.listen(3000, function() {
-    console.log("Server started on port 3000");
-  });
+  console.log("Server started on port 3000");
+});
